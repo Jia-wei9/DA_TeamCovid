e44bb4b62892b284ae9bab90c3f58ed7c4ae4ba6
Cancerttarius <99726683+Cancerttarius@users.noreply.github.com>
2022-02-16 20:52:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import socket
 import requests
+
+url = 'https://www.facebook.com'
+r = requests.get(url)
+print(r.text)
+
+h = requests.head(url)
+print("Header:")
+print("**********")
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
+
 s = socket.socket()
 print("Socket created")
-port= 80
-s.bind(('',port))
-print("Socket bind to %s"%(port))
+port = 8080
+s.bind(('', port))
+print("Socket bind to %s" % (port))
 s.listen(5)
 print("Socket is listening...")
 while True:
-    c, addr=s.accept()
-    print("Connection from",addr)
-    
-    #testtest
+    c, addr = s.accept()
+    print("Connection from", addr)
