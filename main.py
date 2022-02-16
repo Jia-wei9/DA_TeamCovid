2b8edf486e0cdd108a4b11be2a6dc2049bbb996a
Jia-wei9 <99749723+Jia-wei9@users.noreply.github.com>
2022-02-16 18:25:20 +0800

    main.py file
    project file

---
+++ main.py
@@ -0,0 +1,11 @@
+import socket
+s = socket.socket()
+print("Socket created")
+port= 80
+s.bind(('',port))
+print("Socket bind to %s"%(port))
+s.listen(5)
+print("Socket is listening...")
+while True:
+    c, addr=s.accept()
+    print("Connection from",addr)
