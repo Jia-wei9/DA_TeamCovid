a2ee439b6f9e38e022ebb7db096c4f912b020248
Jia-wei9 <99749723+Jia-wei9@users.noreply.github.com>
2022-02-16 21:57:15 +0800

    Working update

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 import socket
 import sys
-import argparse
-
-
-def ploadconnect(websiteB, portN):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("Socket created succefully")
-    except socket.error as err:
-        print("Socket creation failed with erorr %s" % (err))
-
-    port = portN
 
 try:
-    host_ip=socket.gethostbyname(website)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket created successfully")
+except socket.error as err:
+    print("Socket creation failed with error %s" % (err))
+
+port=80
+try:
+    host_ip = socket.gethostbyname("www.facebook.com")
 except socket.gaierror:
-    print("There is a problem resolving the host")
     sys.exit()
-    s.connect((host_ip, port))
-    print("Succesfully connected to the website given %s:%s" % (host_ip, ports))
+s.connect((host_ip, port))
+print("Successfully connect to the website given %s:%s" % (host_ip, port))
 
-praser = argparse.ArgumentParser(description="Learn to connect to a website with port number")
-praser.add_argument("-H", "--host", required=True, help="specify the hostname or website name")
-praser.add_argument("-p", "-port", type=int, default=80, help="specify the port number eg. 80 for website")
+s = socket.socket()
+print("Socket created")
+s.bind(('', port))
+print("Socket bind to %s" % (port))
+s.listen(5)
+print("Socket is listening...")
+while True:
+    c, addr = s.accept()
+    print("Connection from", addr)
 
-args = praser.parse_args()
-ploadconnect(args.host, args.port)
+import argparse
+
+def ploadconnect(websiteN, portN):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Socket created successfully")
+    except socket.error as err:
+        print("Socket creation failed with error %s"%(err))
+
+    ports= portN
+
+    try:
+        host_ip =socket.gethostbyname(websiteN)
+    except socket.gaierror:
+        print("There is a problem resolving the host")
+        sys.exit()
+        s.connect((host_ip,ports))
+        print("Succesfully connected to the website given %s:%s"%(host_ip,ports))
+
+praser=argparse.ArgumentParser(description="Learn to connect to a website with port number")
+praser.add_argument("-H","--host",required=True, help="specify the hostname or website name")
+praser.add_argument("-p","-port",type=int, default=80, help="specify the port number eg. 80 for website")
+
+args=praser.parse_args()
+ploadconnect(args.host,args.port)
