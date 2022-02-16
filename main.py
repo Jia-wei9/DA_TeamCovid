ea1412ee31a9feefe876e1a1a7c0611db7605bf9
Cancerttarius <99726683+Cancerttarius@users.noreply.github.com>
2022-02-16 21:10:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import socket
 import requests
-
-url = 'https://www.facebook.com'
+# Task 5i
+url = 'https://www.abweb.biz'
 r = requests.get(url)
 print(r.text)
-
+# Task 5ii
+print("Status code:")
+print("\t *", r.status_code, "OK")
+# Task 5iii
 h = requests.head(url)
 print("Header:")
 print("**********")
 for x in h.headers:
     print("\t ", x, ":", h.headers[x])
 print("**********")
-
+# Task iv
+headers = {
+    'User-Agent': 'mobile'
+    }
+url2 = 'https://www.abweb.biz/'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
+# Task ?
 s = socket.socket()
 print("Socket created")
 port = 8080
